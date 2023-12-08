0202a9b65dd39f01bff33cc6dcff529e19d8c323
AbhilakshSinghReen <abhilakshreen@gmail.com>
2023-12-08 14:40:13 +0530

    Added the report model

--- radio_reports/settings.py
+++ radio_reports/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "radio_reports_api",
 ]
 
 MIDDLEWARE = [
--- radio_reports_api/models.py
+++ radio_reports_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    report_id = models.CharField(max_length=255)
+    meshes_metadata = models.TextField()
+    simplified_reports = models.TextField()
+    processing_status = models.TextField()
